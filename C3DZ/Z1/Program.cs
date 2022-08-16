b54df26440b7b4331c81cd81d33a56a0e451a417
Gidrak <paskark@gmail.com>
2022-08-16 18:46:52 +0300

    ту би айм нот туби

---
+++ C3DZ/Z1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 23432 -> да
+// 12821 -> да
+
+Console.WriteLine("Введите пятизначное число: ");
+string? xyzab = Console.ReadLine();
+
+void CN_xyzab(string xyzab)
+{
+    if (xyzab[0]==xyzab[4] && xyzab[1]==xyzab[3])
+    {
+        Console.WriteLine($"Вы угадали: {xyzab} -> является палиндром (да)");
+    }
+    else
+    {
+        Console.WriteLine($"Вы не угадали: {xyzab} -> является палиндром (нет)");
+    }
+}
+if (xyzab!.Length == 5)
+{
+    CN_xyzab(xyzab);
+}
+else
+{
+    Console.WriteLine($"Ошибка ВводаБ попробуйте снова ™ + © + € + ‡");
+}
