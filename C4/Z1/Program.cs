9153b19c68437fc6d677fa64a28a055a0b0a714d
Gidrak <paskark@gmail.com>
2022-08-17 00:09:21 +0300

    т

---
+++ C4/Z1/Program.cs
@@ -0,0 +1,26 @@
+﻿int num = GetNumberFromUser("Введите целое число A: ","Ошибка ввода!");
+Console.WriteLine("Количество цифр в числе");
+int sumNumbers = GetSumNumbers(num);
+Console.WriteLine($"\t{num} -> {sumNumbers}");
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+     while(true)
+     {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if(isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+     } 
+}
+int GetSumNumbers(int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        sum ++;
+        number /= 10;  
+    } 
+    return sum;  
+}
