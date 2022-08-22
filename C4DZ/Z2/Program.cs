cdf8c7d497cd8dcc448a8acf9ba5f23bdba73418
Gidrak <paskark@gmail.com>
2022-08-22 19:07:41 +0300

    бадaбум

---
+++ C4DZ/Z2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Clear();
+
+Console.Write("Введите число : ");
+int digit = Convert.ToInt32(Console.ReadLine());
+
+  int SumDigit(int digit)
+  { 
+    int counter = Convert.ToString(digit).Length;
+    int advance = 0;
+    int result = 0;
+
+    for (int i = 0; i < counter; i++)
+    {
+      advance = digit - digit % 10;
+      result = result + (digit - advance);
+      digit = digit / 10;
+    }
+   return result;
+  }
+
+int sumDigit = SumDigit(digit);
+Console.WriteLine("Сумма цифр в числе: " + sumDigit);
