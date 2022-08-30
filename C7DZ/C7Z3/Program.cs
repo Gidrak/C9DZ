23041044caab5ececf8eacf74eebe87893a44773
Gidrak <paskark@gmail.com>
2022-08-30 19:27:43 +0300

    Как так?

---
+++ C7DZ/C7Z3/Program.cs
@@ -0,0 +1,61 @@
+﻿//Задача 49: Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.
+//Например, изначально массив выглядел вот так: 
+// 1 4 7 2      
+// 5 9 2 3      
+// 8 4 2 4      
+// Новый массив будет выглядеть вот так:
+// 1 4 7 2
+// 5 81 2 9
+// 8 4 2 4
+
+Console.Clear();
+
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine() ?? "");
+
+int[,] array = GetArray(rows, columns, 0, 100);
+
+PrintArray(array);
+
+PrintArrayInSquare(array);
+
+PrintArray(array);
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+void PrintArrayInSquare(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            if ((i % 2 == 1) && (j % 2 == 1))
+            inArray[i, j] = inArray[i, j] * inArray[i, j];
+        }
+    }
+    Console.WriteLine();
+}
