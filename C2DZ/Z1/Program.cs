b4cc1ea6b4ace493171c41cda74d934dc8ed8c1f
Gidrak <110244187+Gidrak@users.noreply.github.com>
2022-09-04 15:27:30 +0300

    Delete C2DZ directory

--- C2DZ/Z1/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
-Console.Clear();
-
-Console.Write("Введите трёхзначное число: ");
-int A1 = Convert.ToInt32(Console.ReadLine());
-string B1 = Convert.ToString(A1);
-Console.WriteLine("Покажем вторую цифру введенного числа: " + A1);
-Console.WriteLine("вторая цифра введенного числа -> "+ B1[1]);
